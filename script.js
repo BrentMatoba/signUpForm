758ac9b5f3da014241375d9aef9732fc0642acfc
BrentMatoba <118773387+BrentMatoba@users.noreply.github.com>
2023-12-04 23:34:41 -0800

    added password check functionality

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("passwordConfirm");
+const submitButton = document.getElementById("submitButton");
+
+
+function checkPass(){
+    let value1 = password.value;
+    let value2 = passwordConfirm.value;
+    if(value1 == value2){
+        console.log("yay!");
+    }
+    else{
+        password.classList.add("error");
+        passwordConfirm.classList.add('error');
+        
+    }
+}
+
+submitButton.addEventListener("click", checkPass);
